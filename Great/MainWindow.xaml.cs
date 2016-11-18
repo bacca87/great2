06d75dc70e4386990393ee815dce7da39c0d2e6e
Marco Baccarani <bacca87@gmail.com>
2016-11-19 00:11:50 +0100

    First Commit

---
+++ Great/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Fluent;
+using Great.DB;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Great
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : RibbonWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RibbonWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            IList<WorkingDay> days = new List<WorkingDay>();
+            DateTimeFormatInfo dfi = DateTimeFormatInfo.CurrentInfo;
+            System.Globalization.Calendar cal = dfi.Calendar;
+
+            using (var db = new DBEntities())
+            {
+                foreach (DateTime date in AllDatesInMonth(2016, 11))
+                {
+                    try
+                    {
+                        WorkingDay day = new WorkingDay { WeekNr = cal.GetWeekOfYear(date, dfi.CalendarWeekRule, dfi.FirstDayOfWeek), Day = date, Timesheets = db.Timesheet.Where(t => t.Date == date.Date).ToList() };
+                        days.Add(day);
+                    }
+                    catch (Exception ex)
+                    {
+                        ex.ToString();
+                    }
+                }
+            }
+            
+            workingDaysDataGrid.ItemsSource = days;
+        }
+
+        public static IEnumerable<DateTime> AllDatesInMonth(int year, int month)
+        {
+            int days = DateTime.DaysInMonth(year, month);
+            for (int day = 1; day <= days; day++)
+            {
+                yield return new DateTime(year, month, day);
+            }
+        }
+    }
+}
---
+++ Great/WorkingDay.cs
@@ -0,0 +1,49 @@
+﻿using Great.DB;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Great
+{
+    public class WorkingDay
+    {
+        public int WeekNr { get; set; }
+        public DateTime Day { get; set; }
+        public TimeSpan WorkingTime 
+        {
+            get
+            {
+                TimeSpan totalTime = new TimeSpan(0,0,0);
+
+                if (Timesheets == null || Timesheets.Count == 0)
+                    return totalTime;
+
+                foreach(Timesheet ts in Timesheets)
+                {
+                    totalTime += (ts.TravelEndTimeAM.Value - ts.TravelStartTimeAM.Value) +
+                                 (ts.TravelEndTimePM.Value - ts.TravelStartTimePM.Value) +
+                                 (ts.WorkEndTimeAM.Value - ts.WorkStartTimeAM.Value) +
+                                 (ts.WorkEndTimePM.Value - ts.WorkStartTimePM.Value);
+                }
+
+                return totalTime;
+            }
+        }
+        public TimeSpan TravelTime { get; set; }
+        public int Overtime34 { get; set; }
+        public int Overtime50 { get; set; }
+        public int Overtime85 { get; set; }
+        public int Overtime100 { get; set; }
+
+        #region Display Properties
+        public string DayString { get { return Day.ToLongDateString(); } }
+        public string WorkingTimeString { get { return WorkingTime.ToString(@"hh\:mm"); } }
+        public string TravelTimeString { get { return TravelTime.ToString(@"hh\:mm"); } }
+        #endregion
+
+        public IList<Timesheet> Timesheets { get; set; }
+    }
+}
