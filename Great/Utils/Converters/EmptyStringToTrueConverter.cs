26eae80fa81acda007ee7ed7e5cc402c172e6c23
Marco Baccarani <bacca87@gmail.com>
2018-12-05 17:11:12 +0100

    EA #8
    user interaction improved

---
+++ Great/Utils/Converters/EmptyStringToTrueConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Great.Utils.Converters
+{
+    public class EmptyStringToTrueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(value != null && (value as string) != string.Empty);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Great/Utils/Converters/ZeroDoubleToNullConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Great.Utils.Converters
+{
+    public class ZeroDoubleToNullConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null && value is double && (double)value == 0)
+                return null;
+            else
+                return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
