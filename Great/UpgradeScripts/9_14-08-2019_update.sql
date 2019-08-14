5900c3f68ef9c735468ba019c6fea4a7a3adf6f6
Marco Baccarani <bacca87@gmail.com>
2019-08-14 11:05:42 +0200

    clean wrong notifyasnew flags on existing DB

---
+++ Great/UpgradeScripts/9_14-08-2019_update.sql
@@ -0,0 +1,13 @@
+﻿--=========================================================================
+-- Date: 13/08/2019
+-- Description: fix EA refunds
+-- Author: Marco Baccarani
+--=========================================================================
+
+UPDATE FDL SET NotifyAsNew = 0 WHERE [Status] > 0;
+UPDATE ExpenseAccount SET NotifyAsNew = 0 WHERE [Status] > 0;
+
+--=========================================================================
+-- MANDATORY: Increment internal db version
+PRAGMA user_version = 9;
+--=========================================================================
