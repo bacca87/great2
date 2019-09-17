99b07cf630d0742e4efaba34ae8efe6c9ed1c422
Marco Baccarani <bacca87@gmail.com>
2019-09-17 14:47:29 +0200

    bugfix flag iscompiled

---
+++ Great/UpgradeScripts/0012_17-09-2019_update.sql
@@ -0,0 +1,13 @@
+﻿--=========================================================================
+-- Date: 17/09/2019
+-- Description: fix iscompiled fields on imported fdl and ea
+-- Author: Marco Baccarani
+--=========================================================================
+
+UPDATE FDL SET IsCompiled = 1 WHERE IsReadOnly = 1 AND [Status] = 2 OR [Status] = 3;
+UPDATE ExpenseAccount SET IsCompiled = 1 WHERE IsReadOnly = 1 AND [Status] = 2 OR [Status] = 3;
+
+--=========================================================================
+-- MANDATORY: Increment internal db version
+PRAGMA user_version = 12;
+--=========================================================================
