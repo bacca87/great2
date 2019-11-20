7e215fdd1c8de5b4f9d9fcc512da6b1e61c2d5ac
Marco Baccarani <bacca87@gmail.com>
2019-11-20 11:13:39 +0100

    description fixed

--- Great/ViewModels/InformationsViewModel.cs
+++ Great/ViewModels/InformationsViewModel.cs
@@ -58,7 +58,7 @@ namespace Great2.ViewModels
         public InformationsViewModel()
         {
             ProductDescription = "This software has been designed to ease the business traveler's tasks.\n" +
-                "Keep track of you worked hours, expense accounts, travels and check your stats to be always informed about your activities.\n" +
+                "Keep track of your worked hours, expense accounts, travels and check your stats to be always informed about your activities.\n" +
                 "All of this in automatic way!" +
                 "Please feel free to contribute to this project helping us to add new feature and keep everithing up to date, just visit our website for download the source code and start to improve it!\n\n" +
                 "A special thanks to Andrea 'Cina' Ghinelli, the original ideator of this very useful software!";
